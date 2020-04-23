2a059a328e55510901549a3e76f7abd987244946
stefanomelchior <stefano.melchior@gmail.com>
2020-04-23 16:37:52 +0200

    Fine tuning system.img files and anbox to be run locally
    Change-Id: I3412c2ce56a0d68ffbe74c16a95fbd4277e3cf5b

--- system/halium/anbox.sh
+++ system/halium/anbox.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # sudo apt install anbox-ubuntu-touch
 # mkdir anbox-data
-# sudo wget -q --show-progress -O /home/phablet/anbox-data/android.img http://cdimage.ubports.com/anbox-images/android-armhf-64binder.img
+# wget -q --show-progress -O /home/phablet/anbox-data/android.img http://cdimage.ubports.com/anbox-images/android-armhf-64binder.img
 # touch /home/phablet/anbox-data/.enable
 # mkdir anbox-data/data
 # sudo chmod -R o+wrx /home/phablet/anbox-data/data
